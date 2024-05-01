3906d83b1899bec015ef26f9e66bb54ac2514938
Szymon Makuła <szymakula@gmail.com>
2024-05-01 08:56:49 +0200

    add sdp module

--- src/http.rs
+++ src/http.rs
@@ -1,39 +1,131 @@
 use std::fmt::{Display, Formatter};
-use std::io::Error;
-use std::num::ParseIntError;
-use std::string::FromUtf8Error;
+use std::io::ErrorKind;
 
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
+use tokio::sync::mpsc::Sender;
 
-pub async fn handle_http_request(mut stream: TcpStream, fingerprint: &str) {
-    match parse_http_request(&mut stream).await {
-        Some(req) => {
-            println!("got req {}", req);
-            match &req.path[..] {
-                "/whip" => {
-                    match &req.method {
-                        HTTPMethod::POST => {}
-                        _ => {
-                            if let Err(err) = write_405_response(&mut stream).await {
-                                eprint!("Error writing a HTTP response {}", err)
+use crate::ice_registry::Session;
+
+pub struct HTTPServer {
+    fingerprint: String,
+    session_commands_sender: Sender<SessionCommand>,
+}
+
+
+impl HTTPServer {
+    pub fn new(fingerprint: String, sender: Sender<SessionCommand>) -> Self {
+        HTTPServer {
+            fingerprint,
+            session_commands_sender: sender,
+        }
+    }
+    pub async fn handle_http_request(&self, mut stream: TcpStream) {
+        match parse_http_request(&mut stream).await {
+            Some(req) => {
+                println!("got req {}", req);
+                match &req.path[..] {
+                    "/whip" => {
+                        match &req.method {
+                            HTTPMethod::POST => {
+                                if let Err(e) = self.register_streamer(&mut stream, req.body) {
+                                    eprint!("Error writing a HTTP response {}", e)
+                                }
+                            }
+                            _ => {
+                                if let Err(err) = write_405_response(&mut stream).await {
+                                    eprint!("Error writing a HTTP response {}", err)
+                                }
                             }
                         }
                     }
-                }
-                _ => {
-                    if let Err(err) = write_404_response(&mut stream).await {
-                        eprint!("Error writing a HTTP response {}", err)
+                    _ => {
+                        if let Err(err) = write_404_response(&mut stream).await {
+                            eprint!("Error writing a HTTP response {}", err)
+                        }
                     }
+                };
+            }
+            None => {
+                if let Err(err) = write_400_response(&mut stream).await {
+                    eprint!("Error writing a HTTP response {}", err)
                 }
-            };
-        }
-        None => {
-            if let Err(err) = write_400_response(&mut stream).await {
-                eprint!("Error writing a HTTP response {}", err)
             }
         }
     }
+
+    fn register_streamer(&self, stream: &mut TcpStream, body: Option<String>) -> std::io::Result<()> {
+        let body = body.ok_or(ErrorKind::Other)?;
+        let sdp = parse_sdp(body);
+        println!("sdp {:?}", sdp);
+
+        Ok(())
+    }
+}
+
+const ICE_USERNAME_ATTRIBUTE_PREFIX: &str = "a=ice-ufrag:";
+const ICE_PASSWORD_ATTRIBUTE_PREFIX: &str = "a=ice-pwd:";
+const GROUP_ATTRIBUTE_PREFIX: &str = "a=group:";
+const MEDIA_LINE_PREFIX: &str = "m=";
+
+const WHITELISTED_ATTRIBUTES: [&str; 8] = ["m=", "a=ssrc", "a=msid", "a=rtcp-mux", "a=rtpmap", "a=fmtp", "a=mid", "a=rtcp"];
+
+fn parse_sdp(data: String) -> Option<SDP> {
+    let mut lines = data.lines();
+    let remote_username = lines.clone().find(|line| line.starts_with(ICE_USERNAME_ATTRIBUTE_PREFIX)).and_then(|line| line.split_once(":").map(|line| line.1))?.to_owned();
+    let remote_password = lines.clone().find(|line| line.starts_with(ICE_PASSWORD_ATTRIBUTE_PREFIX)).and_then(|line| line.split_once(":").map(|line| line.1))?.to_owned();
+    let bundle = lines.clone().find(|line| line.starts_with(GROUP_ATTRIBUTE_PREFIX)).and_then(|line| line.split_once(":").map(|line| line.1))?.to_owned();
+
+    let mut media_lines = lines.skip_while(|line| !line.starts_with(MEDIA_LINE_PREFIX)).filter(|line| WHITELISTED_ATTRIBUTES.iter().any(|item| line.starts_with(item)));
+
+    let mut media_descriptors = vec![vec![]];
+    let mut media_index = 0;
+    media_descriptors[media_index].push(media_lines.next()?);
+    while let Some(line) = media_lines.next() {
+        if line.starts_with("m=") {
+            media_index += 1;
+            media_descriptors.push(vec![])
+        }
+        media_descriptors[media_index].push(line)
+    }
+
+    let media_descriptors = media_descriptors.into_iter().map(|descriptor| {
+        let mut iterator = descriptor.into_iter();
+        let media_attribute: Vec<&str> = iterator.next()?.splitn(4, " ").collect();
+        let media_type = media_attribute[0].split_once("=")?.1.to_owned();
+
+        let attributes = iterator.map(|str| str.to_owned()).collect::<Vec<String>>();
+        Some(MediaDescription {
+            media_type,
+            protocol: media_attribute[2].to_owned(),
+            format: media_attribute[3].to_owned(),
+            attributes,
+        })
+    }).collect::<Option<Vec<MediaDescription>>>()?;
+
+
+    Some(SDP {
+        ice_pwd: remote_password,
+        ice_username: remote_username,
+        group: bundle,
+        media_descriptions: media_descriptors,
+    })
+}
+
+#[derive(Debug)]
+struct SDP {
+    ice_username: String,
+    ice_pwd: String,
+    group: String,
+    media_descriptions: Vec<MediaDescription>,
+}
+
+#[derive(Debug)]
+struct MediaDescription {
+    media_type: String,
+    protocol: String,
+    format: String,
+    attributes: Vec<String>,
 }
 
 pub async fn handle_whip_request(request: Request, fingerprint: &str) -> Result<String, HttpError> {
@@ -166,23 +258,6 @@ enum HttpError {
     MalformedRequest,
 }
 
-impl From<FromUtf8Error> for HttpError {
-    fn from(value: FromUtf8Error) -> Self {
-        HttpError::MalformedRequest
-    }
-}
-
-impl From<std::io::Error> for HttpError {
-    fn from(value: Error) -> Self {
-        HttpError::MalformedRequest
-    }
-}
-
-impl From<ParseIntError> for HttpError {
-    fn from(value: ParseIntError) -> Self {
-        HttpError::MalformedRequest
-    }
-}
 
 impl std::fmt::Display for HttpError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -194,4 +269,9 @@ impl std::fmt::Display for HttpError {
             HttpError::MalformedRequest => write!(f, "405 Malformed request"),
         }
     }
+}
+
+pub enum SessionCommand {
+    AddStreamer(Session),
+    AddViewer(Session),
 }
