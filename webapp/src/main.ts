6f333e6fe829592a4605376cf24c38d443532803
Szymon Makuła <szymakula@gmail.com>
2024-06-02 21:51:31 +0200

    save progress

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,16 +1,23 @@
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 import {html, LitElement} from "lit";
 import MainStyles from "./styles/main.css" assert {type: "css"}
+import "./lobby"
+import "./stream"
 
 @customElement('s-main')
 export class Main extends LitElement {
     static styles = MainStyles;
 
+    @property() search = window.location.search
+
+
     render() {
+        const searchTarget = new URLSearchParams(window.location.search).get("watch")
+        if (searchTarget) return html`
+            <s-stream streamID=${searchTarget}></s-stream>`
+
         return html`
-            <main>
-                <header>hell2o</header>
-                <div>hello</div>
-            </main>`;
+            <s-rooms></s-rooms>`;
     }
 }
+
