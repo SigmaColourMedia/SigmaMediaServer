e392a80c3761a71c989863ec74249c11fd388923
nynon <szymakula@gmail.com>
2024-07-09 18:22:39 +0200

    handle rtpmap and fmtp

--- media_server/src/sdp_2.rs
+++ media_server/src/sdp_2.rs
@@ -83,6 +83,18 @@ fn parse_attribute(attribute: &str) -> Result<Attribute, SDPParseError> {
             let value = value.ok_or(SDPParseError::MalformedAttribute)?;
             Ok(Attribute::MediaGroup(value))
         }
+        "rtpmap" => {
+            let value = value
+                .ok_or(SDPParseError::MalformedAttribute)
+                .and_then(|val| parse_rtpmap(&val))?;
+            Ok(Attribute::RTPMap(value))
+        }
+        "fmtp" => {
+            let value = value
+                .ok_or(SDPParseError::MalformedAttribute)
+                .and_then(|val| parse_fmtp(&val))?;
+            Ok(Attribute::FMTP(value))
+        }
         "rtcp-mux" => Ok(Attribute::RTCPMux),
         _ => Ok(Attribute::Unrecognized),
     }
@@ -154,6 +166,42 @@ fn parse_fingerprint(input: &str) -> Result<Fingerprint, SDPParseError> {
     })
 }
 
+fn parse_rtpmap(input: &str) -> Result<RTPMap, SDPParseError> {
+    let (payload_number, codec) = input
+        .split_once(" ")
+        .ok_or(SDPParseError::MalformedAttribute)?;
+
+    let media_codec = match codec.to_ascii_lowercase().as_str() {
+        "h264/90000" => MediaCodec::Video(VideoCodec::H264),
+        "opus/48000/2" => MediaCodec::Audio(AudioCodec::OPUS),
+        _ => return Err(SDPParseError::UnsupportedCodec),
+    };
+
+    let payload_number = payload_number
+        .parse::<usize>()
+        .map_err(|_| SDPParseError::MalformedAttribute)?;
+
+    Ok(RTPMap {
+        codec: media_codec,
+        payload_number,
+    })
+}
+
+fn parse_fmtp(input: &str) -> Result<FMTP, SDPParseError> {
+    let (payload_number, capabilities) = input
+        .split_once(" ")
+        .ok_or(SDPParseError::MalformedAttribute)?;
+
+    let payload_number = payload_number
+        .parse::<usize>()
+        .map_err(|_| SDPParseError::MalformedAttribute)?;
+
+    Ok(FMTP {
+        format_capability: capabilities.to_string(),
+        payload_number,
+    })
+}
+
 #[derive(Debug)]
 struct SDPParseErrorStackTrace {
     error: SDPParseError,
@@ -168,6 +216,7 @@ enum SDPParseError {
     MalformedMediaDescriptor,
     MalformedSDPLine,
     UnsupportedHashFunction,
+    UnsupportedCodec,
 }
 #[derive(Debug)]
 enum SDPLine {
@@ -197,7 +246,6 @@ enum Attribute {
     RTPMap(RTPMap),
     FMTP(FMTP),
     Candidate(Candidate),
-    RTCPFeedback(RTCPFeedback),
 }
 #[derive(Debug)]
 struct MediaDescription {
@@ -230,7 +278,25 @@ enum HashFunction {
 }
 
 #[derive(Debug)]
-struct RTPMap {}
+struct RTPMap {
+    codec: MediaCodec,
+    payload_number: usize,
+}
+
+#[derive(Debug)]
+enum MediaCodec {
+    Video(VideoCodec),
+    Audio(AudioCodec),
+}
+#[derive(Debug)]
+enum VideoCodec {
+    H264,
+}
+
+#[derive(Debug)]
+enum AudioCodec {
+    OPUS,
+}
 
 #[derive(Debug)]
 struct MediaSSRC {
@@ -238,13 +304,14 @@ struct MediaSSRC {
 }
 
 #[derive(Debug)]
-struct FMTP {}
+struct FMTP {
+    payload_number: usize,
+    format_capability: String,
+}
 
 #[derive(Debug)]
 struct Candidate {}
 
-#[derive(Debug)]
-enum RTCPFeedback {}
 const EXAMPLE_SDP: &str = "v=0\r\no=rtc 3767197920 0 IN IP4 127.0.0.1\r\ns=-\r\nt=0 0\r\na=group:BUNDLE 0 1\r\na=group:LS 0 1\r\na=msid-semantic:WMS *\r\na=setup:actpass\r\na=ice-ufrag:E2Fr\r\na=ice-pwd:OpQzg1PAwUdeOB244chlgd\r\na=ice-options:trickle\r\na=fingerprint:sha-256 EF:53:C9:F2:E0:A0:4F:1D:5E:99:4C:20:B8:D7:DE:21:3B:58:15:C4:E5:88:87:46:65:27:F7:3B:C6:DC:EF:3B\r\nm=audio 4557 UDP/TLS/RTP/SAVPF 111\r\nc=IN IP4 192.168.0.198\r\na=mid:0\r\na=sendonly\r\na=ssrc:1349455989 cname:0X2NGAsK9XcmnsuZ\r\na=ssrc:1349455989 msid:qUVEoh7TF9nLCrk4 qUVEoh7TF9nLCrk4-audio\r\na=msid:qUVEoh7TF9nLCrk4 qUVEoh7TF9nLCrk4-audio\r\na=rtcp-mux\r\na=rtpmap:111 opus/48000/2\r\na=fmtp:111 minptime=10;maxaveragebitrate=96000;stereo=1;sprop-stereo=1;useinbandfec=1\r\na=candidate:1 1 UDP 2015363327 192.168.0.198 4557 typ host\r\na=candidate:2 1 UDP 2015363583 fe80::6c3d:5b42:1532:2f9a 10007 typ host\r\na=end-of-candidates\r\nm=video 4557 UDP/TLS/RTP/SAVPF 96\r\nc=IN IP4 192.168.0.198\r\na=mid:1\r\na=sendonly\r\na=ssrc:1349455990 cname:0X2NGAsK9XcmnsuZ\r\na=ssrc:1349455990 msid:qUVEoh7TF9nLCrk4 qUVEoh7TF9nLCrk4-video\r\na=msid:qUVEoh7TF9nLCrk4 qUVEoh7TF9nLCrk4-video\r\na=rtcp-mux\r\na=rtpmap:96 H264/90000\r\na=rtcp-fb:96 nack\r\na=rtcp-fb:96 nack pli\r\na=rtcp-fb:96 goog-remb\r\na=fmtp:96 profile-level-id=42e01f;packetization-mode=1;level-asymmetry-allowed=1\r\n";
 
 #[cfg(test)]
