84d977ea88a4eb9782ce1c4d8b976b272db681dd
nynon <szymakula@gmail.com>
2024-07-14 21:21:53 +0200

    add streamer sdp parser

--- media_server/src/sdp_2.rs
+++ media_server/src/sdp_2.rs
@@ -3,6 +3,79 @@ use std::io::BufRead;
 use std::net::IpAddr;
 use std::str::FromStr;
 
+fn parse_streamer_sdp_offer(offer: SDPOffer) -> Result<ResolvedSDP, StreamerOfferSDPParseError> {
+    let find_payload_number_by_media_codec =
+        |media_attributes: &Vec<Attribute>, media_codec: MediaCodec| {
+            media_attributes
+                .iter()
+                .find_map(|attr| match attr {
+                    Attribute::RTPMap(rtpmap) => {
+                        if matches!(&rtpmap.codec, media_codec) {
+                            return Some(rtpmap.payload_number);
+                        }
+                        return None;
+                    }
+                    _ => None,
+                })
+                .ok_or(StreamerOfferSDPParseError::UnsupportedMediaCodecs)
+        };
+
+    let supported_video_codec_payload_number = find_payload_number_by_media_codec(
+        offer.video_media_description.as_ref(),
+        MediaCodec::Video(VideoCodec::H264),
+    )?;
+    let supported_audio_codec_payload_number = find_payload_number_by_media_codec(
+        offer.audio_media_description.as_ref(),
+        MediaCodec::Audio(AudioCodec::Opus),
+    )?;
+
+    let supported_video_fmtp = offer
+        .video_media_description
+        .iter()
+        .find_map(|attr| match attr {
+            Attribute::FMTP(fmtp) => {
+                if fmtp
+                    .payload_number
+                    .eq(&supported_video_codec_payload_number)
+                {
+                    return Some(fmtp.clone());
+                }
+                return None;
+            }
+            _ => None,
+        })
+        .ok_or(StreamerOfferSDPParseError::MissingVideoProfileSettings)?;
+
+    let video_ssrc = offer
+        .video_media_description
+        .iter()
+        .find_map(|attr| match attr {
+            Attribute::MediaSSRC(ssrc) => Some(ssrc.clone()),
+            _ => None,
+        })
+        .ok_or(StreamerOfferSDPParseError::MissingRemoteSSRC)?;
+    let audio_ssrc = offer
+        .audio_media_description
+        .iter()
+        .find_map(|attr| match attr {
+            Attribute::MediaSSRC(ssrc) => Some(ssrc.clone()),
+            _ => None,
+        })
+        .ok_or(StreamerOfferSDPParseError::MissingRemoteSSRC)?;
+
+    Ok(ResolvedSDP {
+        audio_codec: AudioCodec::Opus,
+        remote_ice_username: offer.ice_username,
+        remote_ice_password: offer.ice_password,
+        video_capability: supported_video_fmtp,
+        audio_payload_number: supported_audio_codec_payload_number,
+        video_payload_number: supported_video_codec_payload_number,
+        video_codec: VideoCodec::H264,
+        remote_video_ssrc: video_ssrc,
+        remote_audio_ssrc: audio_ssrc,
+    })
+}
+
 fn parse_raw_sdp_offer(data: &str) -> Result<SDPOffer, SDPParseError> {
     let sdp_lines = data
         .lines()
@@ -365,22 +438,24 @@ struct SDPOffer {
     video_media_description: Vec<Attribute>,
 }
 
-struct StreamerSDP {
-    ice_username: String,
-    ice_password: String,
+struct ResolvedSDP {
+    remote_ice_username: String,
+    remote_ice_password: String,
     video_codec: VideoCodec,
+    video_payload_number: usize,
     audio_codec: AudioCodec,
-    video_ssrc: MediaSSRC,
-    audio_ssrc: MediaSSRC,
-    audio_capability: FMTP,
+    audio_payload_number: usize,
+    remote_video_ssrc: MediaSSRC,
+    remote_audio_ssrc: MediaSSRC,
     video_capability: FMTP,
 }
 
-struct ViewerSDP {
-    ice_username: String,
-    ice_password: String,
-    resolved_video_payload_number: usize,
-    resolved_audio_payload_number: usize,
+enum StreamerOfferSDPParseError {
+    UnsupportedMediaCodecs,
+    UnsupportedProfileSettings,
+    MissingVideoProfileSettings,
+    MissingRemoteSSRC,
+    UnsupportedMediaDirection,
 }
 
 #[derive(Debug)]
