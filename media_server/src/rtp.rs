87ac375c78099c4a693e23357e5dff639958952a
szymakula <szymakula@gmail.com>
2024-08-14 16:11:28 +0200

    fix rtp

--- media_server/src/rtp.rs
+++ media_server/src/rtp.rs
@@ -64,16 +64,16 @@ fn get_mapped_header(
     }
 }
 
-struct RTPHeader {
+pub struct RTPHeader {
     marker_set: bool,
-    payload_type: u8,
+    pub payload_type: u8,
     ssrc: u32,
 }
-fn get_rtp_header_data(buffer: &[u8]) -> RTPHeader {
+pub fn get_rtp_header_data(buffer: &[u8]) -> RTPHeader {
     let first_byte = buffer[1];
 
     let marker_set = (first_byte & 0b1000_0000) == 0b1000_0000;
-    let payload_type = first_byte ^ 0b0000_0000;
+    let payload_type = first_byte & 0b0111_1111;
     let ssrc = NetworkEndian::read_u32(&buffer[8..12]);
 
     RTPHeader {
