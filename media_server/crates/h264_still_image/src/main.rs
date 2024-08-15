7562e0023649f7eb54216cfa2b0b2af82c7342c1
szymakula <szymakula@gmail.com>
2024-08-15 11:59:27 +0200

    feat: add rtp packet parser

--- media_server/crates/h264_still_image/src/main.rs
+++ media_server/crates/h264_still_image/src/main.rs
@@ -1,72 +1,21 @@
-use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::Read;
 
-use byteorder::{ByteOrder, NetworkEndian};
+use byteorder::ByteOrder;
+
+use crate::rtp_dump::get_rtp_packets;
+
+mod rtp;
+mod rtp_dump;
 
 fn main() {
     let rtp_packets = get_rtp_packets();
-    println!("packets {}", rtp_packets.len())
-}
 
-/**
-RTP-dump format:
-- File starts with utf-8 encoded string `#!rtpplay1.0 address/port\n`
-- Followed by RD Header
-
- 0                   1                   2                   3
- 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|             Start of recording GMT seconds                    |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|             Start of recording GMT microseconds               |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|             Network source                                    |
-+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
-|          port                 |            padding            |
-+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
-
-- Then each RTP packet is followed by RD_T header
- 0                   1                   2                   3
- 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|  RTP packet length + header   |         RTP packet length     |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                  Offset                                       |
-+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
- */
-
-fn get_rtp_packets() -> Vec<Vec<u8>> {
-    let rtp_dump = File::open("../../wireshark-dump.rtp").unwrap();
-    let mut reader = BufReader::new(rtp_dump);
-    let mut rtp_dump_header = vec![0u8; RTP_DUMP_HEADER_LEN + RD_HEADER_LEN]; // skip heading string + RT_D header
-    reader.read_exact(&mut rtp_dump_header).unwrap();
-
-    let mut rt_header_buffer = vec![0u8; 8];
-    let mut rtp_buffer = vec![0u8; 3000];
-
-    let mut rtp_packets = Vec::with_capacity(3000);
-
-    while let Ok(_) = reader.read_exact(&mut rt_header_buffer) {
-        let rt_header = get_rt_header(&rt_header_buffer);
-        rtp_buffer.resize(rt_header.rtp_length as usize, 0);
-
-        reader.read_exact(&mut rtp_buffer).unwrap();
-        rtp_packets.push(rtp_buffer.clone())
+    for packet in rtp_packets {
+        println!("packet {:?}", packet);
+        break;
     }
-
-    rtp_packets
 }
 
-fn get_rt_header(buffer: &[u8]) -> RTHeader {
-    let rtp_length = NetworkEndian::read_u16(&buffer[2..4]);
-    RTHeader { rtp_length }
-}
-
-static RTP_DUMP_HEADER_LEN: usize = 28;
-static RD_HEADER_LEN: usize = 16;
-struct RTHeader {
-    rtp_length: u16,
-}
 // let mut out = fs::read("../../wireshark2.bin").unwrap();
 //
 // let config = DecoderConfig::new().debug(true);
